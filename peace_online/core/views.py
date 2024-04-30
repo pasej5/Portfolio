bbce03b477ebb3203ef046279668942033488f84
Jealous Matsikachando <pasej5@gmail.com>
2024-04-30 23:41:07 +0200

    images added

--- peace_online/core/views.py
+++ peace_online/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 # Create your views here.
 def index(request):
     """information about the browser"""
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    
+    context = {'categories': categories, 'items': items}
+    return render(request, 'core/index.html', context)
 
 
 def contact(request):
