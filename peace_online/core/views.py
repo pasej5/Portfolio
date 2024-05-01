3a9a3dedae56010021cc89f5ab347a0fe659a382
Jealous Matsikachando <pasej5@gmail.com>
2024-05-02 00:33:57 +0200

    models update, Items added

--- peace_online/core/views.py
+++ peace_online/core/views.py
@@ -7,7 +7,8 @@ def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
     
-    return render(request, 'core/index.html', {'categories': categories, 'items': items})
+    context = {'categories': categories, 'items': items}
+    return render(request, 'core/index.html', context)
 
 
 def contact(request):
--- peace_online/peace_online/urls.py
+++ peace_online/peace_online/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
@@ -25,4 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name="contact")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
