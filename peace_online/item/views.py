f04b19542e81e6d4203980c9f1495a3559807269
Jealous Matsikachando <pasej5@gmail.com>
2024-05-02 03:06:58 +0200

    more detail to items

--- peace_online/item/views.py
+++ peace_online/item/views.py
@@ -4,7 +4,7 @@ from .models import Item
 
 # Create your views here.
 def detail(request, pk):
-    item = get_object_or_404(item, pk=pk)
+    item = get_object_or_404(Item, pk=pk)
     
     context = {'item': item}
     return render(request, 'item/detail.html', context)
--- peace_online/peace_online/urls.py
+++ peace_online/peace_online/urls.py
@@ -23,6 +23,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
 
