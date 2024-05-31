fbefb9f7c0c9e88857cbabd9de3b6b597359f4a3
Jealous Matsikachando <pasej5@gmail.com>
2024-05-31 09:48:33 +0200

    adding a new decorator for a new item to be added

--- peace_online/item/views.py
+++ peace_online/item/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
+from .form import NewItemForm
 from .models import Item
 
 # Create your views here.
@@ -9,3 +11,11 @@ def detail(request, pk):
     
     context = {'item': item, 'related_items': related_items}
     return render(request, 'item/detail.html', context)
+
+
+@login_required
+def new(request):
+    form = NewItemForm()
+    
+    return render(request, 'item/form.html')
+    
