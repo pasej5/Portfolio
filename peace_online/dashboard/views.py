f4bdac759606ed0da28778d317a77d00a63f0c83
Jealous Matsikachando <pasej5@gmail.com>
2024-05-31 12:41:07 +0200

    index.html added

--- peace_online/dashboard/views.py
+++ peace_online/dashboard/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from item.models import Item
+
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)  # get all the items you have created
+    
+    #then render the template
+    context = {'items': items}
+    return render(request, 'dashboard/index.html')
+    
