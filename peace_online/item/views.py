5e06e20ee1c58c94f1733903416f1826c7a0c6a3
Jealous Matsikachando <pasej5@gmail.com>
2024-05-31 12:07:26 +0200

    check if the form is valid

--- peace_online/item/views.py
+++ peace_online/item/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .form import NewItemForm
 from .models import Item
@@ -15,7 +15,18 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-    form = NewItemForm()
+    if request.method =="POST":
+        
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item= form.save(commit=False)# this will create a database but not save it
+            item.created_by = request.user # this time the user is alwaya authenticated because we have the @login required
+            item.save()
+            
+            return redirect('item:detail', pk=item.id) #redirect the user back to the page just created
+    else:
+        form = NewItemForm()
+        
     context = {'form': form, 'title': 'New item'}
     return render(request, 'item/form.html', context)
     
