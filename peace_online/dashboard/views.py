616d845d92ff29e84b966fa12718504ad3491ca1
Jealous Matsikachando <pasej5@gmail.com>
2024-06-08 04:10:40 +0200

    login

--- peace_online/dashboard/views.py
+++ peace_online/dashboard/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from item.models import Item
 
 @login_required
-def index(request):
+def index(request):# request stands for HTTP request what the user is requesting or sending to the backend
     items = Item.objects.filter(created_by=request.user)  # get all the items you have created
     
     #then render the template
